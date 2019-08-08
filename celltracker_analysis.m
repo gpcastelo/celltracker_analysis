3c53b45d6ea1ff4296d7353fa2a028dbad71f19d
Guillermo Prol Castelo <guillermoprolcastelo@gmail.com>
2019-08-08 11:28:17 +0200

    Tracks with proper format

--- celltracker_analysis.m
+++ celltracker_analysis.m
@@ -7,10 +7,10 @@ path="C:\Users\G-mo10\Desktop\Test images nuclei Agata\CellTracker results\";
 filename = "TracksCoordinates_C0506A1R3_007.mat";
 filename=path+filename;
 load(filename);
-cells=pos(end:end);
-matrix_x=nan(frames,cells); matrix_y=matrix_x;
+CELLS=pos(end:end);
+matrix_x=nan(frames,CELLS); matrix_y=matrix_x;
 %%%%%%%Fills matrices of positions: cell 0 | cell 1 | cell 2 | etc.:%%%%%%
-for jj=1:cells
+for jj=1:CELLS
     kk=1;
 for ii=1:2:length(pos)
     if pos(ii,4)==jj
@@ -22,7 +22,7 @@ end
 end
 %%%%%%%Plots positions:%%%%%%%%%
 % sz=100; a = (1:frames)'; b = num2str(flipud(a)); c = cellstr(b);
-% for ll=1:cells
+% for ll=1:CELLS
 % scatter(matrix_x(:,ll),matrix_y(:,ll),sz,'filled')
 % text(matrix_x(:,ll)-3.5, matrix_y(:,ll)+.5, c,'Color','white','FontWeight','bold')
 % hold on
@@ -37,15 +37,35 @@ end
 %%%%%%%Separate tracks between short and long:
 %Get the length of each track:
 track_lengths=[];
-for ll=1:cells
+for ll=1:CELLS
 single_track=length(matrix_x(:,ll))-sum(isnan(matrix_x(:,ll)));
 track_lengths=[track_lengths single_track];
 end
 mean_track=mean(track_lengths);
-histogram(track_lengths)
-title('Length in frames of tracks')
-xlabel('Length (# of frames)'); ylabel('Count')
+% histogram(track_lengths)
+% title('Length in frames of tracks')
+% xlabel('Length (# of frames)'); ylabel('Count')
 
 %%%%%%%%%%%%%%%%%%%% Velocities: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 vel_x=diff(matrix_x)/dt;vel_y=diff(matrix_y)/dt; %microns/s
 %vel_x=matrix_x*3600;vel_y=matrix_y*3600;        %microns/h
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%% MSD: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SPACE_UNITS = 'µm';
+TIME_UNITS = 's';
+tracks = cell(CELLS, 1);
+for mm = 1 : CELLS
+    % Time
+    time = (0 : frames-1)' * dt;
+    % Positions
+    X=[matrix_x(:,mm) matrix_y(:,mm)];
+    % Store
+    tracks{mm} = [time X];
+end
+%Initiate msd analyzer (2 means 2D):
+ma = msdanalyzer(2, SPACE_UNITS, TIME_UNITS);
+% This works:
+ma = ma.addAll(tracks);
+% Indeed:
+disp(ma)
