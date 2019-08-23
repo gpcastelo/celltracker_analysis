65f1e2ed7bac6b38d6cd6c0252d60f0c94008874
Guillermo Prol Castelo <guillermoprolcastelo@gmail.com>
2019-08-23 12:39:02 +0200

    Solved the problem where velocities in between frames were not correctly calculated

--- celltracker_analysis.m
+++ celltracker_analysis.m
@@ -9,13 +9,16 @@ filename=path+filename;
 load(filename); save_pos=pos;
 CELLS=pos(end:end);
 matrix_x=nan(frames,CELLS); matrix_y=matrix_x; dummy_frames=nan(frames,CELLS);
-%%%%%%%Fills matrices of positions: cell 1 | cell 2 | cell 3 | etc.:%%%%%%
+%%%%%%%Fills matrices of positions:         cell 1 | cell 2 | cell 3 | etc.:%%%%%%
+%%%%%%% and frames                  frame 1
+%                                   frame 2
+%                                   frame 3 etc.
 for jj=1:CELLS
     kk=1;
     for ii=1:length(pos)
         if pos(ii,4)==jj && mod(pos(ii,3),2)==1
-            matrix_x(kk,jj)=pos(ii,1);%/pixelsize; %in microns
-            matrix_y(kk,jj)=pos(ii,2);%/pixelsize; %in microns
+            matrix_x(ceil(pos(ii,3)/2),jj)=pos(ii,1)/pixelsize; %in microns
+            matrix_y(ceil(pos(ii,3)/2),jj)=pos(ii,2)/pixelsize; %in microns
             dummy_frames(kk,jj)=pos(ii,3);
             kk=kk+1;
         elseif pos(ii,4)==jj+1
@@ -31,15 +34,15 @@ matrix_x_daughters=[]; matrix_y_daughters=[];
 for jj=1:CELLS
     if dummy_frames(1,jj)<=16                               %Primary cells 
                                                             %(first frame 
-                                                            %<=16)
+                                                            %<=8*2)
         matrix_x_primary=[matrix_x_primary matrix_x(:,jj)];
         matrix_y_primary=[matrix_y_primary matrix_y(:,jj)];
     else                                                    %Daughter cells
         matrix_x_daughters=[matrix_x_daughters matrix_x(:,jj)];
         matrix_y_daughters=[matrix_y_daughters matrix_y(:,jj)];
-    end
+     end
 end
-clear ii jj kk
+clear jj
 %%%%%%%Polar coordinates: rho=sqrt(x^2+y^2); phi=atan2(y,x)
 matrix_rho=sqrt(matrix_x.^2+matrix_y.^2); %microns
 [len_x,wid_x]=size(matrix_rho); 
@@ -51,31 +54,18 @@ for ii=1:len_x
 end
 clear ii jj len_x wid_x
 %%%%%%%Fills matrices of positions: frame 1 | frame 2 | frame 3 | etc.:%%%%%%
-matrix_x_for_frameav=nan(CELLS,2*frames); matrix_y_for_frameav=matrix_x_for_frameav;
-for jj=1:2:(2*frames)
-    kk=1;
-for ii=1:length(pos)
-    if pos(ii,3)==jj
-        matrix_x_for_frameav(kk,jj)=pos(ii,1)/pixelsize; %in microns
-        matrix_y_for_frameav(kk,jj)=pos(ii,2)/pixelsize; %in microns
-        kk=kk+1;
-    end
-end
-end
-clear ii jj kk
-%%%%%%%Plots positions:%%%%%%%%%
-% sz=100; a = (1:frames)'; b = num2str(flipud(a)); c = cellstr(b);
-% for ll=1:CELLS
-% scatter(matrix_x(:,ll),matrix_y(:,ll),sz,'filled')
-% text(matrix_x(:,ll)-3.5, matrix_y(:,ll)+.5, c,'Color','white','FontWeight','bold')
-% hold on
-% plot(matrix_x(:,ll),matrix_y(:,ll))
-% hold on
-% %axis([0 200 0 120])
+% matrix_x_for_frameav=nan(CELLS,2*frames); matrix_y_for_frameav=matrix_x_for_frameav;
+% for jj=1:2:frames
+%     kk=1;
+% for ii=1:length(pos)
+%     if pos(ii,3)==jj && mod(pos(ii,3),2)==1
+%         matrix_x_for_frameav(kk,jj)=pos(ii,1)/pixelsize; %in microns
+%         matrix_y_for_frameav(kk,jj)=pos(ii,2)/pixelsize; %in microns
+%         kk=kk+1;
+%     end
 % end
-% title('Cell trajectories')
-% xlabel('Microns'); ylabel('Microns')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% end
+% clear ii jj kk
 
 %%%%%%%Separate tracks between short and long:
 %Get the length of each track:
@@ -107,30 +97,74 @@ vel_cell_avg=nanmean(vel_mag); %average velocity for each cell
 % plot(vel_cell_avg,'b')
 % title('Average velocity for each cell')
 % xlabel('Cell'); ylabel('Velocity (\mu m/s)')
-% % % % % % %For each frame:
-vel_x_frame=diff(matrix_x_for_frameav)/dt; vel_y_frame=diff(matrix_y_for_frameav)/dt;
-vel_frame=sqrt(vel_x_frame.^2+vel_y_frame.^2);
-[~,widmat]=size(matrix_x_for_frameav);
-vel_frame_avg=[];
-for aa=1:2:widmat
-    vel_avg_single_frame=nanmean(vel_frame(:,aa));
-    vel_frame_avg=[vel_frame_avg vel_avg_single_frame];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % % % % % %For each frame of all cells:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% vel_x_frame=diff(matrix_x_for_frameav)/dt; vel_y_frame=diff(matrix_y_for_frameav)/dt;
+% vel_frame=sqrt(vel_x_frame.^2+vel_y_frame.^2);
+% [~,widmat]=size(vel_frame);
+% vel_frame_avg=[];
+% for aa=1:2:widmat
+%     vel_avg_single_frame=nanmean(vel_frame(:,aa));
+%     vel_frame_avg=[vel_frame_avg vel_avg_single_frame];
+% end
+% clear aa widmat vel_avg_single_frame
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%Create matrix of instant velocities:
+vel_x_frame=[]; vel_y_frame=[];
+[len_x,~]=size(matrix_x);
+for aa=2:len_x
+    instant_vel_x=(matrix_x(aa,:)-matrix_x(aa-1,:))/dt;
+    vel_x_frame=[vel_x_frame; instant_vel_x];
+    instant_vel_y=(matrix_y(aa,:)-matrix_y(aa-1,:))/dt;
+    vel_y_frame=[vel_y_frame; instant_vel_y];
 end
-clear aa widmat vel_avg_single_frame
-%disp('Average velocity for each frame:')
-%disp(vel_frame_avg)
+clear aa len_x instant_vel_x instant_vel_y
+vel_all=sqrt(vel_x_frame.^2+vel_y_frame.^2);
+%%From the matrix of instant velocities, create vector of avg velocity:
+avg_vel_all=[];
+[len_x,~]=size(vel_all);
+for aa=1:len_x
+    dummy_vel=nanmean(vel_all(aa,:));
+    avg_vel_all=[avg_vel_all dummy_vel];
+end
+clear aa len_x dummy_vel
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Plot:
+err=std(avg_vel_all)*ones(size(avg_vel_all));
 figure
-plot(1:2:120,vel_frame_avg,'k')
-title('Average velocity for each frame')
+ebar=errorbar(1:59, avg_vel_all, err, 'LineStyle','none');
+ebar.Color = 'k';
+hold on
+scatter(1:59,avg_vel_all,'r','filled')
+title('Average velocity for each frame: All cells')
 xlabel('Frame'); ylabel('Velocity (\mu m/s)')
-dummy_vector_for_avg_vel=[];
-for ii=1:frames-1
-    for jj=1:CELLS
-        dummy_vector_for_avg_vel=[dummy_vector_for_avg_vel vel_mag(ii,jj)];
-    end
-end
-disp('Average velocity of all cells:')
-vel_total_avg=nanmean(dummy_vector_for_avg_vel) %average velocity of all cells
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% For each frame of primary cells:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% vel_x_primary=diff(matrix_x_primary')/dt; vel_y_primary=diff(matrix_y_primary')/dt;
+% vel_x_primary=vel_x_primary'; vel_y_primary=vel_y_primary';
+% vel_frame_primary=sqrt(vel_x_frame.^2+vel_y_frame.^2);
+% [~,widmat]=size(vel_frame_primary); 
+% vel_primary_avg=[];
+% for aa=1:2:widmat
+%     vel_avg_single_frame=nanmean(vel_frame_primary(:,aa));
+%     vel_primary_avg=[vel_primary_avg vel_avg_single_frame];
+% end
+% clear aa widmat vel_avg_single_frame
+% figure
+% scatter(1:60,vel_primary_avg,'b')
+% title('Average velocity for each frame: Primary cells')
+% xlabel('Frame'); ylabel('Velocity (\mu m/s)')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dummy_vector_for_avg_vel=[];
+% for ii=1:frames-1
+%     for jj=1:CELLS
+%         dummy_vector_for_avg_vel=[dummy_vector_for_avg_vel vel_mag(ii,jj)];
+%     end
+% end
+% disp('Average velocity of all cells:')
+% vel_total_avg=nanmean(dummy_vector_for_avg_vel) %average velocity of all cells
 clear ii jj dummy_vector_for_avg_vel
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
